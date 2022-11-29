b7870b03d82aefb8c08607695c7ecb96aa4e48d2
jvargsal <jvargsal@emeal.ntttdata.com>
2022-11-29 12:01:58 -0500

    Lectura Perfil Usuario - Result fixed

--- app/src/main/java/com/bootcampnttdata6/plantshost/util/Result.kt
+++ app/src/main/java/com/bootcampnttdata6/plantshost/util/Result.kt
@@ -6,15 +6,3 @@ sealed class Result<out T> {
     data class Success<out T>(val data: T): Result<T>()
     data class Failure(val exception: Exception): Result<Nothing>()
 }
-sealed class Result<out T> {
-    object Loading:Result<Nothing>()
-    object Finish:Result<Nothing>()
-    data class Success<out T>(val data: T): Result<T>()
-    data class Failure(val exception: Exception): Result<Nothing>()
-}
-sealed class Result<out T> {
-    object Loading:Result<Nothing>()
-    object Finish:Result<Nothing>()
-    data class Success<out T>(val data: T): Result<T>()
-    data class Failure(val exception: Exception): Result<Nothing>()
-}
